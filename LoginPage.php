da29628e6994694038c3fe80a3e7885c64f10c6d
thsmmil <thsmmil01@gmail.com>
2022-06-09 04:07:36 -0300

    Ajuste pagina login

--- LoginPage.php
+++ LoginPage.php
@@ -20,36 +20,48 @@
             <div class="col-5 bg-secondary">
                 <div class="d-flex flex-column justify-content-center h-100 w-100">
                     <div class="d-flex flex-column justify-content-center">
-                        <img src="icon.png" width="350" height="250">
-                        <h1 class="text-white text-center">Drivan</h1>
-                        <p class="text-white fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore natus
+                        <div class="text-center">
+                            <img src="./wwwroot/images/icon.png" width="350" height="250">
+                            <h1 class="text-white">Drivan</h1>
+                        </div>
+                        <p class="text-white fs-5 pt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore natus
                             saepe eum ipsa soluta eveniet voluptatem suscipit animi voluptates.</p>
                     </div>
                 </div>
             </div>
             <div class="col-7 min-vh-100">
                 <div class="d-flex flex-column justify-content-center h-100 w-100">
-                    <div class="card py-3 px-4 mx-2">
-                        <form>
-                            <div class="mb-3 mt-3">
-                                <label for="email" class="form-label">E-mail:</label>
-                                <input type="email" class="form-control" id="email" placeholder="Insira seu e-mail"
-                                    name="email">
-                            </div>
-                            <div class="mb-3">
-                                <label for="pwd" class="form-label">Senha:</label>
-                                <input type="password" class="form-control" id="pwd" placeholder="Insira sua senha"
-                                    name="pswd">
-                            </div>
-                            <div class="form-check mb-3">
-                                <label class="form-check-label">
-                                    <input class="form-check-input" type="checkbox" name="remember"> Mantenha-me conectado
-                                </label>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </form>
+                    <div class="card py-3 px-4 mx-2" style="border-radius: 1rem;">
+                        <div class="card-body text-black">
+                            <form>
+                            <h5 class="fw-normal mb-3" style="letter-spacing: 1px;">Acesse a sua conta!</h5>
+                                <div class="mb-4">
+                                    <label for="email" class="form-label">E-mail:</label>
+                                    <input type="email" class="form-control" id="email" placeholder="Insira seu e-mail" name="email">
+                                </div>
+                                <div class="mb-4">
+                                    <label for="pwd" class="form-label">Senha:</label>
+                                    <input type="password" class="form-control" id="pwd" placeholder="Insira sua senha" name="pswd">
+                                </div>
+                                <div class="form-check mb-3">
+                                    <label class="form-check-label">
+                                        <input class="form-check-input" type="checkbox" name="remember"> Mantenha-me conectado
+                                    </label>
+                                </div>
+                                <div class="d-grid gap-2 col-4 me-auto">
+                                    <button type="submit" class="btn btn-primary">Login</button>
+                                </div>
+                                <div class="hr d-flex align-items-center my-4">
+                                    <p class="text-center fw-bold mx-3 mb-0 text-muted">OU</p>
+                                </div>
+                                <p><a href="#!" class="link-info">Cadastra-se aqui (Passageiro)</a></p>
+                                <p><a href="#!" class="link-info">Cadastra-se aqui (Motorista)</a></p>
+
+                            </form>
+                        </div>
+
                     </div>
-                    
+
                 </div>
             </div>
         </div>
